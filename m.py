901cd69c4a7d3af43647c28a240faf80ad982cff
GrawtoG <zsuetam.k@gmail.com>
2026-05-20 09:00:05 +0200

    add print_as_matrix

--- m.py
+++ m.py
@@ -1,7 +1,16 @@
+def print_as_matrix(matrix):
+    for i in range(len(matrix)):
+        print("[ ", end="")
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end=" ")
+        print("]")
+    print("\n")
+
+
 m = [
-    [float('inf'), 5, 4, 6, 6],
-    [8, float('inf'), 5, 3, 4],
-    [4, 3, float('inf'), 3, 1],
-    [8, 2, 5, float('inf'), 6],
-    [2, 2, 7, 0, float('inf')]
+    [float("inf"), 5, 4, 6, 6],
+    [8, float("inf"), 5, 3, 4],
+    [4, 3, float("inf"), 3, 1],
+    [8, 2, 5, float("inf"), 6],
+    [2, 2, 7, 0, float("inf")],
 ]
